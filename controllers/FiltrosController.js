4f0bff8cc9fc974ecd8ca6e1eae2deeec8fc3c59
Lucas Alves <zombnfl@gmail.com>
2023-05-08 17:29:38 -0300

    Resolvendo as saídas das querys pro front

--- controllers/FiltrosController.js
+++ controllers/FiltrosController.js
@@ -10,16 +10,16 @@ class FiltroController {
         console.log(mes);
         console.log(ano);
         console.log("--------------------------------------");
-        const lucroMes = await sequelize.query(`SELECT sum(lucro) AS 'lucro_total_mes' FROM vendas
+        const lucroMes = await sequelize.query(`SELECT sum(lucro) AS 'lucromes' FROM vendas
         WHERE mes_criacao = '${mes}' AND ano_criacao = '${ano}'`);
 
-        const lucroDia = await sequelize.query(`SELECT sum(lucro) AS 'lucro_total_dia' FROM vendas
+        const lucroDia = await sequelize.query(`SELECT sum(lucro) AS 'lucrodia' FROM vendas
         WHERE dia_criacao = '${dia}' AND mes_criacao = '${mes}' AND ano_criacao = '${ano}'`);
 
         const vendasAbertasMes = await sequelize.query(`SELECT count(*) AS 'vendas_abertas' from vendas WHERE status_pedido = 'pendente'
         AND mes_criacao = '${mes}' AND ano_criacao = '${ano}'`);
 
-        const vendasEncerradasMes = await sequelize.query(`SELECT count(*) AS 'vendas_abertas' from vendas WHERE status_pedido = 'ok'
+        const vendasEncerradasMes = await sequelize.query(`SELECT count(*) AS 'vendas_encerradas_mes' from vendas WHERE status_pedido = 'ok'
         AND mes_criacao = '${mes}' AND ano_criacao = '${ano}'`);
 
         if(!lucroMes[0])
@@ -38,16 +38,24 @@ class FiltroController {
             })
         }
 
+        const {lucro_dia} = lucroDia;
+
+        let result = {
+            lucro_dia : lucroDia[0],
+            lucro_mes: lucroMes[0],
+            vendas_abertas: vendasAbertasMes[0],
+            vendas_encerradas_mes: vendasEncerradasMes[0]
+        }
+
+        console.log(result['lucro_dia'][0]['lucrodia']);
+
         return res.status(200).json({
-            resultado: {
-                lucro_dia : lucroDia[0],
-                lucro_mes: lucroMes[0],
-                vendas_abertas: vendasAbertasMes[0],
-                vendas_encerradas_mes: vendasEncerradasMes[0]
+                lucro_dia : result['lucro_dia'][0]['lucrodia'],
+                lucro_mes: result['lucro_mes'][0]['lucromes'],
+                vendas_abertas: result['vendas_abertas'][0]['vendas_abertas'],
+                vendas_encerradas_mes: result['vendas_encerradas_mes'][0]['vendas_encerradas_mes']
             } 
-                
-            
-        })
+        )
     }
 }
 
